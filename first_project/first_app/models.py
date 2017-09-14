39c64fda53566fa49a87f07a3372a3742eb252ed
Harshit Trivedi <u6101164@anu.edu.au>
2017-09-14 12:35:17 +1000

    Added first_project and first_app!

---
+++ first_project/first_app/models.py
@@ -0,0 +1,4 @@
+# Stores application's data models.
+from django.db import models
+
+# Create your models here.
---
+++ first_project/first_app/views.py
@@ -0,0 +1,7 @@
+# Handles requests and returns responses.
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse("<h3>Hello World!</h3>")
